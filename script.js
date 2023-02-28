651bc38d67a6c928939872c58389a0aa18403a69
manhhungit-37 <manhhungit.37@gmail.com>
2023-02-28 15:16:58 +0700

    add new map fl

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const events = document.querySelectorAll(".map-wrapper");
-let place = "";
 
 const mapImageString = `
 <svg xmlns="http://www.w3.org/2000/svg" width="1159" height="857" viewBox="0 0 1159 857" fill="none">
